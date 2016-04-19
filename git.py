767976cff4f2fcc25e9767c9fe8b4b280a26e8f7
MyOwnPortfolio <yuko@ukr.net>
2016-04-19 15:03:04 +0300

    testing git

---
+++ git.py
@@ -0,0 +1,8 @@
+li = [9, 5, 10, -1, 7, 0, 2, 4, 6, 3, 1, -3, 4.5]
+n = 1
+while n < len(li):
+    for i in range(len(li)-n):
+        if li[i] > li[i + 1]:
+            li[i], li[i + 1] = li[i + 1], li[i]
+    n += 1
+print(li)
